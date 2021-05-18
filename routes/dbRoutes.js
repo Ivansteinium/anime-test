643513af53c91fd206d3371afb3a2406a88cc7b7
Ivan <ivane@bbd.co.za>
2021-05-18 17:10:17 +0200

    added db routes

---
+++ routes/dbRoutes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const dbController = require("../controllers/dbControl");
+
+const router = Router();
+
+router.post("/addUser", dbController.add_user);
+router.get("/listanime", dbController.list_anime);
+router.post("/addRating", dbController.add_rating);
+
+module.exports = router;
